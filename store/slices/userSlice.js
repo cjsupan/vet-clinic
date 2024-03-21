406eb9e663070913f247371499c4a743c1351f8e
Cedrick John Supan <93630863+cedricksupan@users.noreply.github.com>
2024-03-21 19:39:22 +0800

    Update API URLs and add console.log statement

--- store/slices/userSlice.js
+++ store/slices/userSlice.js
@@ -2,6 +2,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import moment from "moment";
+import { publicRuntimeConfig } from "../../next.config";
+const { API_URL } = publicRuntimeConfig;
 
 const initialState = {
   data: null,
@@ -113,7 +115,7 @@ export const {
 export const fetch = () => async (dispatch) => {
   try {
     dispatch(fetchStart());
-    const response = await axios.get("http://localhost:3000/api/users");
+    const response = await axios.get(`${API_URL}/api/users`);
     dispatch(fetchSuccess(response.data));
     return response;
   } catch (error) {
@@ -128,10 +130,7 @@ export const create = (payload) => async (dispatch) => {
   try {
     payload.Date_created = date;
     payload.Date_updated = date;
-    const response = await axios.post(
-      "http://localhost:3000/api/users",
-      payload
-    );
+    const response = await axios.post(`${API_URL}/api/users`, payload);
     payload._id = response.data.insertedId;
 
     dispatch(createSuccess(payload));
@@ -167,7 +166,7 @@ export const update = (data) => async (dispatch) => {
   try {
     data.data.Date_updated = date;
     const response = await axios.patch(
-      `http://localhost:3000/api/users/${data.Id}`,
+      `${API_URL}/api/users/${data.Id}`,
       data.data
     );
 
@@ -184,7 +183,7 @@ export const update = (data) => async (dispatch) => {
 export const deleted = (userId) => async (dispatch) => {
   dispatch(deleteStart());
   try {
-    const res = await axios.delete(`http://localhost:3000/api/users/${userId}`);
+    const res = await axios.delete(`${API_URL}/api/users/${userId}`);
     dispatch(deleteSuccess(userId));
     return (res.message = "Successfully Deleted!");
   } catch (error) {
